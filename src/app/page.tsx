5366395309c03ff1dcde612ba0a310825570f8e4
Mingina Mathea <3862039-mmathea1@users.noreply.gitlab.com>
2025-09-11 20:17:16 +0300

    added new project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Mingina Portfolio',
+  description: 'A portfolio website for Mingina',
+};
+
+export default function Home() {
+  return (
+   <div>Hi Mingina</div>
+  );
+}

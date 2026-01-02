89d711693f0f17a6b655f50f8b6a31965cc73299
Mingina <mingina.mathea075@gmail.com>
2026-01-02 14:59:52 +0300

    added basic content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,41 @@
 import { Metadata } from "next";
+import Image from "next/image";
+import minginaLogo from "../../public/images/file.svg";
 
 export const metadata: Metadata = {
-  title: 'Mingina Portfolio',
-  description: 'A portfolio website for Mingina',
+  title: "Mingina Portfolio",
+  description: "A portfolio website for Mingina",
 };
 
 export default function Home() {
   return (
-   <main>
-    <nav>
-      <ol>
-        <li>Home</li>
-        <li>About</li>
-        <li>Portfolio</li>
-        <li>Blog</li>
-        <li>Contact</li>
-      </ol>
-    </nav>
-   </main>
+    <div>
+      <main>
+        <nav>
+          <Image src={minginaLogo} alt="Mingina Logo" width="40" height="40" />
+          <ol>
+            <li>workwithme@mingina.com</li>
+            <li>Nairobi, Kenya</li>
+          </ol>
+        </nav>
+        <section>
+          <div>
+            <Image
+              src={minginaLogo}
+              height={200}
+              width={200}
+              alt="Mingina Profile Picture"
+            />
+          </div>
+          <div>
+            <h1>Hi, I am Mingina</h1>
+            <h2>tagline</h2>
+            <p>short descriotion</p>
+          </div>
+          <div>social icons</div>
+        </section>
+      </main>
+      <footer>mingina.com 2025</footer>
+    </div>
   );
 }

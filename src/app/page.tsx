24be7afa64b028c87ef077d9526abbf28c4fb72b
Mingina <mingina.mathea075@gmail.com>
2026-01-02 16:06:15 +0300

    styling basic flex layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import minginaLogo from "../../public/images/file.svg";
 import { FolderDown, Github, Mail, Twitter } from "lucide-react";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Mingina Portfolio",
@@ -10,17 +11,29 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex">
-      <main>
-        <nav>
-          <Image src={minginaLogo} alt="Mingina Logo" width="40" height="40" />
-          <ol>
-            <li>workwithme@mingina.com</li>
-            <li>Nairobi, Kenya</li>
-          </ol>
-        </nav>
-        <section>
-          <div>
+    <div className="flex items-center m-auto justify-between py-10 flex-col">
+      <main className="container mx-auto items-center">
+        <div className="navbar bg-base-100 shadow-sm">
+          <div className="flex-1">
+            <Link href={"/"} className="btn btn-ghost text-xl">
+              <Image
+                src={minginaLogo}
+                alt="Mingina Logo"
+                width="40"
+                height="40"
+              />
+            </Link>
+          </div>
+
+          <div className="flex-none">
+            <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+              <li>workwithme@mingina.com</li>
+              <li>Nairobi, Kenya</li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex justify-center flex-col items-center gap-4 mt-20">
+          <div className="flex items-center">
             <Image
               src={minginaLogo}
               height={200}
@@ -28,7 +41,7 @@ export default function Home() {
               alt="Mingina Profile Picture"
             />
           </div>
-          <div>
+          <div className="flex items-center flex-col text-center">
             <h1>Hi, I am Mingina</h1>
             <h2>Software Engineer</h2>
             <p>
@@ -36,13 +49,13 @@ export default function Home() {
               evolves with use.
             </p>
           </div>
-          <div>
+          <div className="flex flex-row">
             <Twitter size={24} />
             <Mail size={24} />
             <Github size={24} />
             <FolderDown size={24} />
           </div>
-        </section>
+        </div>
       </main>
       <footer>mingina.com 2025</footer>
     </div>
